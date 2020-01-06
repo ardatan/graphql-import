fc185d0ba31b7897ac1040a5ba163e1485e48896
Arda TANRIKULU <20847995+ardatan@users.noreply.github.com>
2020-01-06 16:56:05 +0300

    1.0.0-beta.1 (#420)
    * Different output types
    
    * Bump toolkit
    
    * Release

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { GitLoader } from '@graphql-toolkit/git-loader'
 import { GithubLoader } from '@graphql-toolkit/github-loader'
 import { ApolloEngineLoader } from '@graphql-toolkit/apollo-engine-loader'
 import { PrismaLoader } from '@graphql-toolkit/prisma-loader'
-import { print, BuildSchemaOptions } from 'graphql'
+import { print, BuildSchemaOptions, DocumentNode, GraphQLSchema } from 'graphql'
 import { mergeTypeDefs } from '@graphql-toolkit/schema-merging'
 
 const DEFAULT_SCHEMA_LOADERS = [
@@ -21,25 +21,44 @@ const DEFAULT_SCHEMA_LOADERS = [
   new PrismaLoader()
 ]
 
-export const importSchema = async (
+export async function importSchema(
+  schema: string,
+  options: BuildSchemaOptions & Partial<LoadTypedefsOptions<CodeFileLoaderOptions>>,
+): Promise<string>
+export async function importSchema(
+  schema: string,
+  options: BuildSchemaOptions & Partial<LoadTypedefsOptions<CodeFileLoaderOptions>>,
+  out: 'string',
+): Promise<string>
+export async function importSchema(
+  schema: string,
+  options: BuildSchemaOptions & Partial<LoadTypedefsOptions<CodeFileLoaderOptions>>,
+  out: 'DocumentNode',
+): Promise<DocumentNode>
+export async function importSchema(
+  schema: string,
+  options: BuildSchemaOptions & Partial<LoadTypedefsOptions<CodeFileLoaderOptions>>,
+  out: 'GraphQLSchema',
+): Promise<GraphQLSchema>
+export async function importSchema(
   schema: string,
   options: BuildSchemaOptions & Partial<LoadTypedefsOptions<CodeFileLoaderOptions>> = {},
   out: 'string' | 'DocumentNode' | 'GraphQLSchema' = 'string',
-) => {
+) {
+
+  const allOptions = {
+    loaders: DEFAULT_SCHEMA_LOADERS,
+    filterKinds: OPERATION_KINDS,
+    sort: false,
+    forceGraphQLImport: true,
+    ...options,
+  }
+
   if (out === 'GraphQLSchema') {
-    return loadSchema(schema, { 
-      loaders: DEFAULT_SCHEMA_LOADERS, 
-      forceGraphQLImport: true,
-      ...options 
-    });
+    return loadSchema(schema, allOptions)
   } else {
-    const results = await loadTypedefs(schema, { 
-      loaders: DEFAULT_SCHEMA_LOADERS, 
-      forceGraphQLImport: true,
-      filterKinds: OPERATION_KINDS,
-      ...options 
-    });
-    const mergedDocuments = mergeTypeDefs(results.map(r => r.document))
+    const results = await loadTypedefs(schema, allOptions)
+    const mergedDocuments = mergeTypeDefs(results.map(r => r.document), { useSchemaDefinition: false })
     if (out === 'DocumentNode') {
       return mergedDocuments
     } else {
